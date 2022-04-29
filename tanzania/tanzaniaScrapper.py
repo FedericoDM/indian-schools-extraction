eb8898c0d5a87d1ca18fd96791564f280f542df2
Federico Dominguez Molina <fd.molina@outlook.com>
2022-04-29 00:48:03 -0500

    Fixed code

---
+++ tanzania/tanzaniaScrapper.py
@@ -0,0 +1,13 @@
+# Objeto - Extractor de Info de Tanzania
+
+import re
+import time
+import requests
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup
+
+
+class TanzaniaScrapper():
+    def __init__(self):
+        
